31e7e0748b71d869560cfeac5a8b3429f9dfbeb1
nathandevx <nathanemtz@gmail.com>
2021-10-22 06:03:37 -0700

    home app templates

--- home/views.py
+++ home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 from sentry_sdk import capture_message
+
+
+class HomeView(TemplateView):
+    template_name = 'home/home.html'
 
 
 def error_400(request, exception=None):
