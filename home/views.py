f29d69522c9b1eacf35307e6333089c95c8a2bd8
nathandevx <nathanemtz@gmail.com>
2021-10-23 03:00:32 -0700

    updating

--- home/views.py
+++ home/views.py
@@ -9,19 +9,19 @@ class HomeView(TemplateView):
 
 def error_400(request, exception=None):
     capture_message("400 error - custom page", level="error")
-    return render(request, 'home/error_pages/400.html')
+    return render(request, 'error_pages/400.html')
 
 
 def error_403(request, exception=None):
     capture_message("403 error - custom page", level="error")
-    return render(request, 'home/error_pages/403.html')
+    return render(request, 'error_pages/403.html')
 
 
 def error_404(request, exception=None):
     capture_message("404 error - custom page", level="error")
-    return render(request, 'home/error_pages/404.html')
+    return render(request, 'error_pages/404.html')
 
 
 def error_500(request, exception=None):
     capture_message("500 error - custom page", level="error")
-    return render(request, 'home/error_pages/500.html')
+    return render(request, 'error_pages/500.html')
