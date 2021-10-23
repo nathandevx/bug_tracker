6bd44bfe923f2ad68fcdc4185de8fe43a345ca0a
nathandevx <nathanemtz@gmail.com>
2021-10-23 03:57:12 -0700

    updated tracker models

--- tracker/models.py
+++ tracker/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.conf import settings
 from django.utils.translation import gettext as _
-from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class TimestampMixin(models.Model):
