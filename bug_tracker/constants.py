f877311031e5c8bd872b4b0bee3da7a27dec2764
nathandevx <nathanemtz@gmail.com>
2021-10-26 05:39:02 -0700

    more constants

--- bug_tracker/constants.py
+++ bug_tracker/constants.py
@@ -1,8 +1,11 @@
 dotenv_path = '/home/brickspy/PycharmProjects/bug_tracker/temp/.env'
 PAG_BY = 2
 TIMESTAMP_EXCLUDE = ['creator', 'updater', 'created_at', 'updated_at']
+
 SUPERUSER = 'Superuser'
 ADMIN = 'Admin'
 DEVELOPER = 'Developer'
 REQUESTER = 'Requester'
 VIEWER = 'Viewer'
+ALL_GROUPS = [SUPERUSER, ADMIN, DEVELOPER, REQUESTER, VIEWER]
+ADMINS = [SUPERUSER, ADMIN]
